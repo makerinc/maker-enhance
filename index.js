4df6cf79dc9e51a7ee82edc6dd6572e62f8213c9
Pavel Linkesch <pavel.linkesch@gmail.com>
2018-03-22 14:42:15 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class MakerEnahance extends React.Component {
+  componentDidMount() {
+    this.addScript();
+  }
+
+  componentDidUpdate() {
+    this.run();
+  }
+
+  addScript() {
+    let script = document.querySelector("#maker-enhance-script");
+
+    if (script) {
+      return this.run();
+    }
+
+    script = document.createElement("script");
+    script.id = "maker-enhance-script";
+    script.src = `https://maker.me/enhance/${this.props.user}.js`;
+    document.head.appendChild(script);
+  }
+
+  run() {
+    if (typeof (window.MakerEmbeds || {}).run !== "function") {
+      return;
+    }
+
+    window.MakerEmbeds.run();
+  }
+
+  render() {
+    const index = this.props.index || 0;
+
+    return (
+      <div className="js-maker-enhance-wrapper">
+        <div
+          id={`js-maker-static-enhance-v1-218c2d7d-62da-499e-9e74-93201e1b3d56-${index}`}
+          className="js-maker-enhance-static-mount"
+          style={{ height: "auto", width: "100%" }}
+        />
+      </div>
+    );
+  }
+}
+
+MakerEnahance.propTypes = {
+  user: PropTypes.string.isRequired
+};
