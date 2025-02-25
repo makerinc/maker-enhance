a507a71f17c3fff1747368e1b05e2180c0d8cd13
Pavel Linkesch <pavel.linkesch@gmail.com>
2025-02-25 19:34:46 +0100

    use different stratefy to prevent rerender during hydration

--- lib/MakerEnhanceClient.d.ts
+++ lib/MakerEnhanceClient.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="react" />
 interface MakerEnhanceClientProps {
     user: string;
     id: string;
--- src/MakerEnhance.tsx
+++ src/MakerEnhance.tsx
@@ -7,11 +7,13 @@ interface MakerEnhanceProps {
   loadingHeight?: number;
 }
 
+const isBrowser = typeof window !== "undefined";
+
 export default function MakerEnhance({
   user,
   instanceId,
   loadingHeight,
-}: MakerEnhanceProps): JSX.Element {
+}: MakerEnhanceProps): JSX.Element | null {
   const scriptSrc = `https://app.maker.co/enhance/${user}.js`;
   const id = `js-maker-static-enhance-v1-218c2d7d-62da-499e-9e74-93201e1b3d56-${
     instanceId || "0"
@@ -27,11 +29,8 @@ export default function MakerEnhance({
     ></div>
   `;
 
-  // https://github.com/facebook/react/issues/10923#issuecomment-338715787
-  // "We won't try to manipulate the tree of a dangerouslySetInnerHTML node on the client. Even if it is wrong."
-  //
-  // Use dangerouslySetInnerHTML to avoid issues with hydration.
-  return (
+  // If component was rendered on server, don't rerender on client
+  return isBrowser && document.getElementById(id) ? null : (
     <>
       <MakerEnhanceClient user={user} id={id} scriptSrc={scriptSrc} />
       <div
